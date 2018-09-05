a4950b8dc26334923308870e0108a1d74c100821
Romeo Gono <romeogono1@aol.com>
2018-09-05 10:16:47 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Time.new(:now)
+
+    if time < 12:00:00
+        resp.write "Good Morning!"
+    else
+        resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+
+end
